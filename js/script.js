6d5eb1b194502c6db10f6baade8513b2f89bb910
Orkhan <gurbanlee@gmail.com>
2020-09-29 13:25:15 +0400

    ready for owl

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-function findHome() {
-  let fh = document.querySelector();
-}
+// OWLCAROUSEL START
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false
+      }
+    }
+  });
+});
+
+// OWLCAROUSEL END
