5b8e5dcb2e15b0bd2f7f87361886b6882f1686a8
Gurbanli <orkhandg@code.edu.az>
2020-10-03 16:27:04 +0400

    our team is ready

--- js/script.js
+++ js/script.js
@@ -35,7 +35,22 @@ $(document).ready(function () {
     },
   });
 
-  $(".owl-sidebar").owlCarousel();
+  $(".owl-four").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
   // OWLCAROUSEL END
 
   $(".fh-dropdown").click(function () {
